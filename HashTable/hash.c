d207f8afbb0370924ac3e9edcd7b9cd37be1b4c2
Alex Coll <alex3yoyo@pal-nat184-222-228.itap.purdue.edu>
2015-03-23 11:11:55 -0400

    Added hash table

---
+++ HashTable/hash.c
@@ -0,0 +1,33 @@
+#include "hash.h"
+
+typedef union key {
+    int i;
+    double f;
+    char s[MAX_STR_LENGTH];
+} Key;
+
+int hash (Key key, int n) {
+    if (key.i != 0) {
+        // Key is integePr
+        return key.i % n;
+    } else if (key.f != 0) {
+        // Key is a double
+        return n;
+    } else if (key.s != NULL) {
+        // Key is a string
+        char * p = key.s;
+        int sum = 0;
+        int pos = 1;
+        while (*p) {
+            sum += *p * pos;
+            pos++;
+            p++;
+        }
+        return sum % n;
+    }
+    return -1;
+}
+
+int main () {
+    return 0;
+}
