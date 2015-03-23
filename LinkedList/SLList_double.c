ab7dc57d124b47a2495a29aaf4d185a03de369c6
Alex Coll <alex3yoyo@pal-nat184-222-228.itap.purdue.edu>
2015-03-23 11:11:32 -0400

    Added linked lists

---
+++ LinkedList/SLList_double.c
@@ -0,0 +1,363 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "SLList_double.h"
+
+//
+// Initialize a linked list
+//
+void llist_init(LinkedList * list)
+{
+	list->head = NULL;
+}
+
+//
+// It prints the elements in the list in the form:
+// 4, 6, 2, 3, 8,7
+//
+void llist_print(LinkedList * list) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		printf("{EMPTY}\n");
+		return;
+	}
+
+	printf("{");
+
+	e = list->head;
+	while (e != NULL) {
+		printf("%f", e->value);
+		e = e->next;
+		if (e != NULL) {
+			printf(", ");
+		}
+	}
+	printf("}\n");
+}
+
+//
+// Appends a new node with this value at the beginning of the list
+//
+void llist_add(LinkedList * list, double value) {
+	// Create new node
+	ListNode * n = (ListNode *) malloc(sizeof(ListNode));
+	n->value = value;
+
+	// Add at the beginning of the list
+	n->next = list->head;
+	list->head = n;
+}
+
+//
+// Returns true if the value exists in the list.
+//
+int llist_exists(LinkedList * list, double value) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		// list is empty
+		return 0;
+	}
+
+	e = list->head;
+	while (e != NULL) {
+		if ((e->value) == value) {
+			return 1;
+		}
+		e = e->next;
+	}
+	return 0;
+}
+
+//
+// It removes the entry with that value in the list.
+//
+int llist_remove(LinkedList * list, double value) {
+	ListNode * e;
+	ListNode * previous;
+
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	e = list->head;
+	previous = list->head;
+	// Check first value
+	if (e->value == value) {
+		list->head = e->next;
+		return 1;
+	}
+	// Check all other values
+	while (e->next != NULL) {
+		e = e->next;
+		if (e->value == value) {
+			previous->next = e->next;
+			return 1;
+		}
+		previous = e;
+	}
+	return 0;
+}
+
+//
+// It stores in *value the value that correspond to the ith
+// entry.
+// It returns 1 if success or 0 if there is no ith entry.
+//
+int llist_get_ith(LinkedList * list, double ith, double * value) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	double n = 0.0;
+	e = list->head;
+	while (e != NULL) {
+
+		if (n == ith) {
+			*value = e->value;
+			return 1;
+		}
+		e = e->next;
+		n++;
+	}
+	return 0;
+}
+
+//
+// It removes the ith entry from the list.
+// It returns 1 if success or 0 if there is no ith entry.
+//
+int llist_remove_ith(LinkedList * list, double ith) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	double n = 0;
+	e = list->head;
+	while (e != NULL) {
+		if (n == ith) {
+			return llist_remove(list, e->value);
+		}
+		e = e->next;
+		n++;
+	}
+	return 0;
+}
+
+//
+// It returns the number of elements in the list.
+//
+int llist_number_elements(LinkedList * list) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	double n = 0;
+	e = list->head;
+	while (e != NULL) {
+		e = e->next;
+		n++;
+	}
+	return n;
+}
+
+
+//
+// It saves the list in a file called file_name. The format of the
+// file is as follows:
+//
+// value1\n
+// value2\n
+// ...
+//
+int llist_save(LinkedList * list, char * file_name) {
+	// FIXME
+	ListNode * e = list->head;
+	FILE * fp;
+	fp = fopen(file_name, "w+");
+	if (fp == NULL) {
+  		return 0;
+	}
+	// e = list->head;
+	while (e != NULL) {
+		fprintf(fp, "%f\n", e->value);
+		e = e->next;
+	}
+	fclose(fp);
+	return 0;
+}
+
+//
+// It reads the list from the file_name indicated. If the list
+// already has entries, it will clear the entries.
+//
+int llist_read(LinkedList * list, char * file_name) {
+	FILE * fp = fopen(file_name, "r");
+	if (fp == NULL) {
+  		return 0;
+	}
+	llist_clear(list);
+	char * buffer = (char*) malloc(8 * sizeof(char));
+	while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+		llist_add(list, atof(buffer));
+	}
+	fclose(fp);
+	return 1;
+}
+
+
+//
+// It sorts the list. The parameter ascending determines if
+// the order is ascending (1) or descending(0).
+//
+void llist_sort(LinkedList * list, double ascending) {
+	// FIXME
+	ListNode * e;
+	double done = 0.0;
+	if (list->head == NULL) {
+		return;
+	}
+
+	while (!done) {
+		done = 1;
+		e = list->head;
+		while (e->next != NULL) {
+			if (ascending == 1) {
+				if (e->value > e->next->value) {
+					done = 0;
+					double temp = e->value;
+					e->value = e->next->value;
+					e->next->value = temp;
+				}
+			} else {
+				if (e->value < e->next->value) {
+					done = 0;
+					double temp = e->value;
+					e->value = e->next->value;
+					e->next->value = temp;
+				}
+			}
+			e = e->next;
+		}
+	}
+
+
+	//if (ascending) {
+	//	e = list->head;
+	//    return;
+	//    while (e->next != NULL) {
+	//    	if (e->value > e->next->value) {
+	//    		temp = e->value;
+	//    		e->value = e->next->value;
+	//    		e->next->value = temp;
+	//    		e = e->next;
+	//		}
+	//		}
+	//
+	//} else if (!ascending) {
+	//	return;
+	//} else {
+	//	return;
+	//}
+}
+
+//
+// It removes the first entry in the list and puts value in
+// *value. It also frees memory allocated for the node
+//
+int llist_remove_first(LinkedList * list, double * value) {
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	ListNode * e;
+	e = list->head;
+	*value = e->value;
+	list->head = e->next;
+	e = NULL;
+	return 1;
+}
+
+//
+// It removes the last entry in the list and puts value in
+// *value. It also frees memory allocated for node.
+//
+int llist_remove_last(LinkedList * list, double *value) {
+  	ListNode * e;
+
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	e = list->head;
+	while (1) {
+		if (e->next == NULL) {
+			break;
+		}
+		e = e->next;
+	}
+	*value = e->value;
+	free(e);
+	e = NULL;
+	return 1;
+}
+
+//
+// Insert a value at the beginning of the list.
+// There is no check if the value exists. The entry is added
+// at the beginning of the list.
+//
+void llist_insert_first(LinkedList * list, double value) {
+	ListNode * n = (ListNode *) malloc(sizeof(ListNode));
+	n->value = value;
+	n->next = list->head;
+	list->head = n;
+}
+
+//
+// Insert a value at the end of the list.
+// There is no check if the name already exists. The entry is
+// added at the end of the list.
+//
+void llist_insert_last(LinkedList * list, double value) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		llist_add(list, value);
+		return;
+	}
+
+	e = list->head;
+	while (1) {
+		if (e->next == NULL) {
+			ListNode * new = (ListNode *) malloc(sizeof(ListNode));
+			e->next = new;
+			new->value = value;
+			new->next = NULL;
+			break;
+		} else {
+			e = e->next;
+		}
+	}
+
+	return;
+}
+
+//
+// Clear all elements in the list and free the nodes
+//
+void llist_clear(LinkedList *list) {
+	ListNode * e;
+
+	if (list->head == NULL) {
+		return;
+	}
+}
